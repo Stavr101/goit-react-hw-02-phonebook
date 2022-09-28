166329e0683964f6e97b184175059738765cc4b9
Stavr101 <Shishkin_SU@ukr.net>
2022-09-28 22:47:12 +0300

    add task 6

--- src/components/Phonebook/ContactsList.jsx
+++ src/components/Phonebook/ContactsList.jsx
@@ -1,9 +1,10 @@
-export default function ContactsList({ items }) {
-  console.log(items);
+export default function ContactsList({ items, removeContact }) {
+  //   console.log(items);
   const elements = items.map(({ name, id, number }) => {
     return (
       <li key={id}>
         {name}: {number}{' '}
+        <button onClick={() => removeContact(id)}>Delete</button>
       </li>
     );
   });
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -6,47 +6,80 @@ import { nanoid } from 'nanoid';
 export default class MyPhonebook extends Component {
   state = {
     contacts: [],
+    filter: '',
   };
 
-  addContact = data => {
+  addContact = contact => {
+    if (this.isDublicate(contact)) {
+      return alert(
+        `${contact.name} - ${contact.number} is already in contacts.`
+      );
+    }
     this.setState(prev => {
-      const newData = {
+      const newContact = {
         id: nanoid(),
-        ...data,
+        ...contact,
       };
-
       return {
-        contacts: [...prev.contacts, newData],
+        contacts: [...prev.contacts, newContact],
       };
     });
   };
+  removeContact = id => {
+    this.setState(prev => {
+      const newContact = prev.contacts.filter(item => item.id !== id);
+      return { contacts: newContact };
+    });
+  };
 
-  // handleChange = event => {
-  //   // console.log(event.target);
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+  isDublicate({ name, number }) {
+    const { contacts } = this.state;
+    const result = contacts.find(
+      item => item.name === name && item.nmber === number
+    );
+    return result;
+  }
 
-  //   const { name, value } = event.target;
-  //   this.setState({ [name]: value });
-  // };
-  // handleSubmit = event => {
-  //   event.preventDefault();
-
-  //   const { name } = this.state;
-  //   // console.log(name);
-  //   this.addContact({ name });
-  //   this.setState({ name: '' });
-  // };
+  getFilteredContacts() {
+    const { contacts, filter } = this.state;
+    if (!filter) {
+      return contacts;
+    }
+    const normalaisedFilter = filter.toLocaleLowerCase();
+    const filteredContacts = contacts.filter(({ name, number }) => {
+      const normalaisedName = name.toLocaleLowerCase();
+      const normalaisedNumber = number.toLocaleLowerCase();
+      const result =
+        normalaisedName.includes(normalaisedFilter) ||
+        normalaisedNumber.includes(normalaisedFilter);
+      return result;
+    });
+    return filteredContacts;
+  }
 
   render() {
-    const { addContact } = this;
-    const { contacts } = this.state;
-    // console.log(contacts);
+    const { addContact, removeContact, handleChange } = this;
+    const { filter } = this.state;
+    const contacts = this.getFilteredContacts();
     return (
       <div>
         <h1>Phonebook</h1>
         <FormAddContact onSubmit={addContact} />
-
-        <h2>Contacts</h2>
-        <ContactsList items={contacts} />
+        <h2>Contacts</h2>{' '}
+        <label htmlFor="filter">
+          Find contacts by name
+          <input
+            type="text"
+            name="filter"
+            onChange={handleChange}
+            value={filter}
+          />
+        </label>
+        <ContactsList items={contacts} removeContact={removeContact} />
       </div>
     );
   }
